2fb18c17dd7d33b2e6ff2aa2a741d4ad8cd8e15b
vicky0700018 <vs2734514@gmail.com>
2025-11-26 20:18:41 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 // use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Products routes
+Route::get('/products/product-list', [ProductController::class, 'product_list'])->name('products.product_list');
+Route::get('/products/show_product_form', [ProductController::class, 'show_product_form'])->name('products.show_product_form');
+Route::post('/products/store-product-data', [ProductController::class, 'store_product_data'])->name('products.store_product_data');
+// Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 
 // Users list route
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
